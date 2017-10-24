6d4fad9978f3ed652f9e102d8f057aa0b05024f7
Brooke <Duckiebeewarren@gmail.com>
2017-10-24 00:18:50 -0400

    updated code

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teeze Burger extra pickles', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krabby Patty', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Burger', false);
