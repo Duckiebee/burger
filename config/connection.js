8a5147fc35bdf6d6289913f626e6ecc5201a20a2
Brooke <Duckiebeewarren@gmail.com>
2017-10-24 16:48:45 -0400

    updating files

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,19 @@
 var mysql = require('mysql');
 
-var connection;
-
-if (process.env.JAWSDB_URL) {
-	// DB is JawsDB on Heroku
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	connection = mysql.createConnection({
-		port: 8889,
-		host: 'localhost',
-		user: 'root',
-		password: 'root',
-		database: 'burgers_db'
-	});
-}
+var connection = mysql.createConnection({
+  port: 3000,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
 
 connection.connect(function(err) {
   if (err) {
-    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    console.error("error connecting: " + err.stack);
     return;
   }
-  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+  console.log("connected as id " + connection.threadId);
 });
-
 
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-var connection = require('./connection.js');
+var connection = require('../config/connection.js');
 
 function printQuestionMarks(num) {
 	var arr = [];
@@ -13,10 +13,21 @@ function printQuestionMarks(num) {
 function objToSql(ob) {
 	var arr = [];
 
-	for (var key in ob) {
-		arr.push(key + "=" + ob[key]);
-	}
-
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+  // translate array of strings to a single comma-separated string
 	return arr.toString();
 }
 
@@ -45,6 +56,8 @@ var orm = {
 		queryString += printQuestionMarks(vals.length);
 		queryString += ") ";
 
+console.log(queryString);
+
 		// Perform the database query
 		connection.query(queryString, vals, function(err, result) {
 			if (err) {
@@ -62,6 +75,8 @@ var orm = {
 		queryString += objToSql(objColVals);
 		queryString += " WHERE ";
 		queryString += condition;
+
+console.log(queryString);
 
 		connection.query(queryString, function(err, result) {
 			if (err) {
